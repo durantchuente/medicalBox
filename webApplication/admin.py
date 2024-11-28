437e03faddef92a8b4307ba1a1483a5816bbdaf8
duran tchuente <duran.tchuente@gmail.com>
2024-11-28 13:43:16 -0400

    add deconnexion

--- webApplication/admin.py
+++ webApplication/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from webApplication.models import Patient, MedicalBox, HourMedication
+from webApplication.models import Patient, MedicalBox, HourMedication, DataMedication
 
-admin.site.register([Patient, MedicalBox, HourMedication])
+admin.site.register([Patient, MedicalBox, HourMedication, DataMedication])
